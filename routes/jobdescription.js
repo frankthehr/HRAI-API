c21b09ba02496d9756b3013b3e430f6b57320f7d
frankthehr <frank.pierce@thehrcompany.ie>
2023-03-09 10:34:04 +0000

    Commented jobdescription route, removed unused logic and altered prompt format

--- routes/jobdescription.js
+++ routes/jobdescription.js
@@ -40,18 +40,7 @@ const getDescription = async function(req, res, next) {
     let location = req.body.location;
     let email = req.body.email;
 
-    // let prompt = `Write a 500 word job description for a ${title} in ${location} with ${years} years of experience with the following headings:
-    // Job Title,
-    // Location,
-    // Job Overview,
-    // Requirements,
-    // Years of Experience,
-    // Contact Details
-    // The contact email is ${email}`;
-
-    // let prompt = `Write a 500 word job description for a ${title} in ${location} with ${years} years of experience and add employer's contact details as ${email}. Return it in JSON format with the following with the following headings as keys: "job_title", "location", "job_overview", "requirements", "years_of_experience", "contact_details"`;
-
-    let prompt = `Write a 500 word job description for a ${title} in ${location} with ${years} years of experience and add employer's contact details as ${email}. Return it in JSON format with the following with the following headings as keys: "job_title", "location", "job_overview", "requirements", "years_of_experience", "contact_details". Make the overview very long. Return the requirements as an array`;
+    let prompt = `Write a 500 word job description for a ${title} in ${location} with ${years} years of experience and add employer's contact details as ${email}. Return it in JSON format with the following with the following headings as keys: "job_title", "location", "job_overview", "requirements", "years_of_experience", "contact_details". Make the overview very long. Return the requirements as an array with at least 8 requirements`;
 
     if (prompt === null) {
       throw new Error("Uh oh, no prompt was provided");
@@ -65,14 +54,21 @@ const getDescription = async function(req, res, next) {
       temperature: 1
     });
 
+    // Assign completion from response to variable
     const completion = response.data.choices[0].text;
 
+    // Remove all newline characters from completion
     let completionCleaned = completion.replace(/[\n\r]/g, '');
 
+    // Remove all characters before the JSON object
     let parsedCompletion = removeLeading(completionCleaned);
 
+    // Remove all characters after the JSON object
+
+    // Convert completion from string representation of JSON to actual JSON
     const completionJSON = JSON.parse(parsedCompletion);
 
+    // Store variables in request
     req.prompt = prompt;
     req.completion = completion;
     req.completionJSON = completionJSON;
@@ -87,20 +83,26 @@ const getDescription = async function(req, res, next) {
 const createPDF = async function(req, res, next) {
   try {
 
+    // Get JSON object
     const jsondata = req.completionJSON;
 
+    // Create puppeteer and page instances
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    // Create PDF content from template and JSON object
     const content = await compilePDf('testtemplate', jsondata);
 
+    // Add content to page
     await page.setContent(content);
 
+    // Format page and store it as variable
     const pdf = await page.pdf({
       format: 'A4',
       printBackground: true
     })
 
+    // Store PDF in request (as data)
     req.pdf = pdf;
 
     console.log('Done');
@@ -118,16 +120,15 @@ router
   .route('/')
   .post([getDescription, createPDF], function(req, res) {
     console.log('Job Description Generated');
-    // const data = {
-    //   prompt: req.prompt,
-    //   completion: req.completion,
-    //   jsonData: req.completionJSON
-    // }
-    // res.send(data);
 
+    // Store PDF as variable
     const pdf = req.pdf;
+
+    // Set response header and content type
     res.header('Content-type', 'application/pdf');
     res.contentType("application/pdf");
+
+    // Send PDF data as response
     res.send(pdf);
   })
 

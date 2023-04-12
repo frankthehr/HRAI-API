b93e316af8a65f7848c4d88b35ddb45afd1f5f90
frankthehr <frank.pierce@thehrcompany.ie>
2023-04-12 17:28:20 +0100

    Error handling

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Error handling middleware
+function errorHandler(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+}
+
 const port = process.env.PORT || 5000;
 
 app.use('/jobdescription', jobDescription);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
